35ceeb7bf49bb71270a5a27e2776cf806ee4f1dc
deboraptor <vdzdebora@gmail.com>
2024-05-15 11:24:10 +0200

    :zap: script qui fonctionne

--- scripts/tabulaire.py
+++ scripts/tabulaire.py
@@ -2,12 +2,35 @@
 Script pour faire la mise en fichier tabulaire pour la suite.
 """
 
+import csv
+
+import pandas as pd
+
 from nettoyage import nettoyer_donnees, scrape_thread
 
 
+def fichier_tabulaire(commentaires):
+    df = pd.DataFrame({
+        "text": commentaires,
+        "label": [""] * len(commentaires)
+    })
+
+    df.insert(0, "", range(len(commentaires)))
+    df.to_csv("commentaires.csv", index=False)
+
+
+def ecrire_fichier_csv(commentaires):
+
+    with open("../data/clean/commentaires.csv", "w", newline="", encoding="utf-8") as f:
+        ecrivain = csv.writer(f)
+        ecrivain.writerow(["", "text", "label"])
+
+        for i, commentaire in enumerate(commentaires):
+            ecrivain.writerow([i, commentaire, ""])
+
 
 if __name__ == "__main__":
-    thread_donnee = scrape_thread("https://www.threads.net/?hl=fr", 5)
+    thread_donnee = scrape_thread("https://www.threads.net/?hl=fr", 30)
     thread_donnee = nettoyer_donnees(thread_donnee)
 
     reponses_nettoyees = thread_donnee["reply"]
@@ -19,3 +42,6 @@ if __name__ == "__main__":
 
     print("Réponses de plus de 5 mots : ", commentaires)
     print("Taille du corpus : ", len(commentaires), " commentaires.")
+
+    fichier_tabulaire(commentaires)
+    ecrire_fichier_csv(commentaires)
