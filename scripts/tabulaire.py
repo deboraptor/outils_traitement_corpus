98dd8806a750d85403b1e1a5ba074475b4d90bca
deboraptor <vdzdebora@gmail.com>
2024-05-18 20:35:48 +0200

    :sparkles: fichier final

--- scripts/tabulaire.py
+++ scripts/tabulaire.py
@@ -1,14 +1,46 @@
 """
 Script pour faire la mise en fichier tabulaire pour la suite.
 """
-
 import pandas as pd
-
-import random
 import subprocess
+import nltk
+
+from transformers import pipeline
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 from nettoyage import nettoyer_donnees, scrape_thread
 
+# nltk.download("stopwords")
+# nltk.download("punkt")
+# nltk.download("wordnet")
+
+
+analyzer = pipeline(
+    task="text-classification",
+    model="botdevringring/fr-naxai-ai-emotion-classification-081808122023",
+    tokenizer="botdevringring/fr-naxai-ai-emotion-classification-081808122023"
+)
+
+label_mapping = {
+    "sadness": 0, 
+    "joy": 1, 
+    "love": 2,  
+    "anger": 3,     
+    "fear": 4,
+    "surprise": 5 
+}
+
+def traiter_texte(texte):
+    """ Effectue la lemmatization et enlève les stop words du texte. """
+    mots = word_tokenize(texte)
+    lemmatizer = WordNetLemmatizer()
+    mots_lemmatises = [lemmatizer.lemmatize(mot) for mot in mots]
+    stop_words = set(stopwords.words("french"))
+    mots_filtres = [mot for mot in mots_lemmatises if mot.lower() not in stop_words]
+    texte_traite = " ".join(mots_filtres)
+    return texte_traite
 
 def fichier_tabulaire(commentaires):
     df = pd.DataFrame({
@@ -17,21 +49,19 @@ def fichier_tabulaire(commentaires):
     })
 
     df.insert(0, "", range(len(commentaires)))
-
-    df["label"] = df.apply(lambda row: random.randint(0, 5), axis=1)
-
+    df["label"] = df["text"].apply(lambda row: label_mapping[analyzer(traiter_texte(row))[0]["label"]])    
     return df
 
-def ecrire_fichier_csv(commentaires):
+def ecrire_fichier_csv(commentaires, output_file):
     df = fichier_tabulaire(commentaires)
-
-    df.to_csv("../data/clean/commentaires.csv", index=False)
-
-    # with open("../data/raw/commentaires.txt", "w") as f:
-    #     subprocess.run(["python3", "tabulaire.py"], stdout=f)
+    print("Écriture des données dans le fichier CSV")
+    with open(output_file, "a", newline="", encoding="utf-8") as f:
+        df.to_csv(f, header=f.tell() == 0, index=False)
 
 if __name__ == "__main__":
-    thread_donnee = scrape_thread("https://www.threads.net/?hl=fr", 25)
+    print("Début du scraping des threads")
+    thread_donnee = scrape_thread("https://www.threads.net/", 50)
+    print("Scraping terminé, début du nettoyage des données")
     thread_donnee = nettoyer_donnees(thread_donnee)
 
     reponses_nettoyees = thread_donnee["reply"]
@@ -44,8 +74,8 @@ if __name__ == "__main__":
     print("Réponses de plus de 5 mots : ", commentaires)
     print("Taille du corpus : ", len(commentaires), " commentaires.")
 
-    fichier_tabulaire(commentaires)
-    ecrire_fichier_csv(commentaires)
-
+    output_file = "../data/clean/commentaires.csv"
+    ecrire_fichier_csv(commentaires, output_file)
+    print("Écriture dans le fichier CSV terminée")
 
     # python3 tabulaire.py > ../data/raw/commentaires.txt
