84d6aca59593ccfa4f272758ba5edaee8a3cdd46
deboraptor <vdzdebora@gmail.com>
2024-05-19 02:45:41 +0200

    :art: formatage avec black

--- scripts/tabulaire.py
+++ scripts/tabulaire.py
@@ -20,20 +20,21 @@ from nettoyage import nettoyer_donnees, scrape_thread
 analyzer = pipeline(
     task="text-classification",
     model="botdevringring/fr-naxai-ai-emotion-classification-081808122023",
-    tokenizer="botdevringring/fr-naxai-ai-emotion-classification-081808122023"
+    tokenizer="botdevringring/fr-naxai-ai-emotion-classification-081808122023",
 )
 
 label_mapping = {
-    "sadness": 0, 
-    "joy": 1, 
-    "love": 2,  
-    "anger": 3,     
+    "sadness": 0,
+    "joy": 1,
+    "love": 2,
+    "anger": 3,
     "fear": 4,
-    "surprise": 5 
+    "surprise": 5,
 }
 
+
 def traiter_texte(texte):
-    """ Effectue la lemmatization et enlève les stop words du texte. """
+    """Effectue la lemmatization et enlève les stop words du texte."""
     mots = word_tokenize(texte)
     lemmatizer = WordNetLemmatizer()
     mots_lemmatises = [lemmatizer.lemmatize(mot) for mot in mots]
@@ -42,21 +43,23 @@ def traiter_texte(texte):
     texte_traite = " ".join(mots_filtres)
     return texte_traite
 
+
 def fichier_tabulaire(commentaires):
-    df = pd.DataFrame({
-        "text": commentaires,
-        "label": [""] * len(commentaires)
-    })
+    df = pd.DataFrame({"text": commentaires, "label": [""] * len(commentaires)})
 
     df.insert(0, "", range(len(commentaires)))
-    df["label"] = df["text"].apply(lambda row: label_mapping[analyzer(traiter_texte(row))[0]["label"]])    
+    df["label"] = df["text"].apply(
+        lambda row: label_mapping[analyzer(traiter_texte(row))[0]["label"]]
+    )
     return df
+
 
 def ecrire_fichier_csv(commentaires, output_file):
     df = fichier_tabulaire(commentaires)
     print("Écriture des données dans le fichier CSV")
     with open(output_file, "a", newline="", encoding="utf-8") as f:
         df.to_csv(f, header=f.tell() == 0, index=False)
+
 
 if __name__ == "__main__":
     print("Début du scraping des threads")
